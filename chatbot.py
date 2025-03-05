b32782f96f87d3dcd7331e76fb27787a57bc6df0
sadman-ruet <sadmanhasib21@gmail.com>
2025-03-05 14:20:31 +0600

    added code of qna with Rag

---
+++ chatbot.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import os
+import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
